85a12a452eddc62c669d3e945b576a8453059be6
austenhasty <hastyae@gmail.com>
2019-05-29 14:54:17 -0700

    still need to add links to itineraries, added red error messages, trip & activity names instead of ids

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,8 @@
 class Itinerary < ApplicationRecord
   belongs_to :activity
   belongs_to :trip
+
+  validates :day, presence: true
+  validates :trip_id, presence: true
+  validates :activity_id, presence: true
 end
